1836ae35d5d24b6de40ee92817bd041319b1237f
DANIELLE PARK <dpark1008@gmail.com>
2019-01-07 04:46:52 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -48,10 +48,13 @@ class Song
   end
 
   def self.new_from_filename(filename)
-    filename_array = filename.split(" - ")
-    artist = Artist.find_or_create_by_name(filename_array[0])
-    genre = Genre.find_or_create_by_name(filename_array[2].split(".mp3")[0])
-    song = Song.new(filename_array[1],artist,genre)
+    file_artist = filename.split(" - ")[0]
+    file_song_name = filename.split(" - ")[1]
+    file_genre_mp3 = filename.split(" - ")[2]
+    file_genre = file_genre_mp3.split(".").first
+    song = Song.new(file_song_name)
+    song.artist = Artist.find_or_create_by_name(file_artist)
+    song.genre = Genre.find_or_create_by_name(file_genre)
     song
   end
 end
