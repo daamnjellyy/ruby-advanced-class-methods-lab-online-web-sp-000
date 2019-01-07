fffb3ddbbf461deb7ce44e6a7c921417fa0702e3
DANIELLE PARK <dpark1008@gmail.com>
2019-01-07 04:48:50 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -48,9 +48,11 @@ class Song
   end
 
   def self.new_from_filename(filename)
-    song = self.new_from_filename(song_with_format)
-    song.save
+    song_array = filename.split(" - ")
+    song_array[1] = song_array[1].chomp(".mp3")
+    song = self.new
+    song.name = song_array[1]
+    song.artist_name = song_array[0]
     song
   end
-  
 end
