8ee50401caa771bb09f76b7b0aeb35b6256f505c
DANIELLE PARK <dpark1008@gmail.com>
2019-01-07 04:49:57 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -55,4 +55,13 @@ class Song
     song.artist_name = array[0]
     song
   end
+
+  def self.create_from_filename(filename)
+  result = self.new_from_filename(filename)
+  song = self.create
+  song.name = result.name
+  song.artist_name = result.artist_name
+  song
+end
+
 end
